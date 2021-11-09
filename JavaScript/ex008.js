7578a21b29a527b9ec847e0c4ec7d53a16121f26
Marcelo Rocha <89150138+rocha19@users.noreply.github.com>
2021-11-08 22:06:36 -0300

    js condicional

---
+++ JavaScript/ex008.js
@@ -0,0 +1,10 @@
+// console.log('O console funcionou corretamente!')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`Digija sempre com o cinto de segurança`)
