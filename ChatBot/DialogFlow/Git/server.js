1c67573b17ec7abc193ce842bcbdff24b85405a6
Marcelo Rocha <89150138+rocha19@users.noreply.github.com>
2021-11-16 17:39:09 -0300

    dialogflow api

--- ChatBot/DialogFlow/Git/server.js
+++ ChatBot/DialogFlow/Git/server.js
@@ -5,16 +5,51 @@
 const express = require('express');
 const app = express();
 
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const projectId = 'cursodialogoflow-dcin';
+const sessionId = uuid.v4();
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId,sessionId);
+console.log(sessionPath);
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.send("Teste Plataforma Umbler");
+    response.send("Teste Plataforma Umbler");
 });
+
+app.post('/detectIntent', function(resquest, response){
+    const query = {
+        session: sessionPath,
+        quaryInput:{
+            text: {
+                text: 'Ola',
+                languageCode: 'pt-br'
+            }
+        }
+    }
+   
+
+    sessionClient.detectIntent(query)
+        .then((res)=>{
+        response.json(res)
+    })
+    .catch(err => console.log(err))
+
+})
 
 // listen for requests :)
 var port = process.env.PORT || 3000;
 const listener = app.listen(port, function() {
-  console.log('Your app is listening on port no ' + listener.address().port);
+    console.log('Your app is listening on port no ' + listener.address().port);
 });
