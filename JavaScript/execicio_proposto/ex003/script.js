ff23e9440b1e676ae9eeabaca5ad531fd3b7ffc9
Marcelo Rocha <89150138+rocha19@users.noreply.github.com>
2021-11-22 19:20:37 -0300

    metade do site

---
+++ JavaScript/execicio_proposto/ex003/script.js
@@ -0,0 +1,25 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    
+    msg.innerHTML = `Agora são ${hora}:${minuto} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'img/fotomanha.png'
+        document.body.style.background = ''
+        // img.setAttribute = 'src', 'photo.png'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'img/fototarde.png'
+        document.body.style.background = '' 
+    } else {
+        //BOA NOITE
+        img.src = 'img/fotonoite.png'
+        document.body.style.background = '' 
+    }
+
+}
